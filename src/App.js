97bae7621bf161bf3e3930121fcd463d09e882fb
Myron <dubtwee@gmail.com>
2019-05-14 13:00:26 -0500

    adding redux and react-redux dependencies and scafolding what redux looks like when used inside your React Application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import mapReduxStateToProps from './modules/mapReduxStateToProps';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      enteredKittyName: ''
+    }
+  }
+  clickMe = (event) => {
+     this.props.dispatch({ type: 'KITTY', kittyName: this.state.enteredKittyName  });
+  }
+
+  noClickMe = (event) => {
+    this.props.dispatch({ type: 'DOGGO' });
+  }
+
+  changeKittyName = (event) => {
+    this.setState({
+      enteredKittyName: event.target.value
+    });
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="App">
+        <h1>React Reducers Intro</h1>
+        <p>Current Kitty Name: {this.props.reduxState.firstReducer.kittyList}</p>
+        <input placeholder="Kitty Name" onChange={this.changeKittyName} />
+        <button onClick={this.clickMe}>Click Me</button>
+        <br />
+        <button onClick={this.noClickMe}>No Click Me</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(mapReduxStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firstReducer = (state, action) => {
+    const kittyName = action.kittyName;
+    if (action.type === 'KITTY') {
+        console.log(`First reducer.`);
+        console.log(`state: `, state);
+        console.log(`action: `, action);
+        console.log(`kittyName: `, kittyName);
+
+        return {
+            kittyList: [
+                ...state.kittyList,
+                kittyName
+            ],
+        };
+    }
+
+    return {
+        ...state
+    };
+};
+
+const secondReducer = (state, action) => {
+    if (action.type === 'DOGGO') {
+        console.log(`Second reducer.`);
+        console.log(`state: `, state);
+        console.log(`action: `, action);
+    }
+    return {};
+}
+
+
+const storeInstance = createStore(
+    combineReducers({
+        firstReducer,
+        secondReducer
+    })
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
