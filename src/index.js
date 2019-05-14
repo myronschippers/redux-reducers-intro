430c5cd25a68661a520db2eb17e3316745409b3b
Myron <dubtwee@gmail.com>
2019-05-14 16:51:14 -0500

    adding middleware for logging out the redux reducers which replaces any console logs that we have in place

--- src/index.js
+++ src/index.js
@@ -3,26 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
 const firstReducer = (state = [], action) => {
-    const kittyName = action.kittyName;
     if (action.type === 'KITTY') {
-        console.log(`First reducer.`);
-        console.log(`state: `, state);
-        console.log(`action: `, action);
-        console.log(`kittyName: `, kittyName);
-
+        const kittyName = action.kittyName;
         return [
                 ...state,
                 kittyName
             ];
     }
 
-    return [
-        ...state
-    ];
+    return state;
 };
 
 const secondReducer = (state = [], action) => {
@@ -44,7 +38,8 @@ const storeInstance = createStore(
     combineReducers({
         firstReducer,
         secondReducer
-    })
+    }),
+    applyMiddleware(logger)
 );
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
